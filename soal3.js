7cd9d23857d7425d0bc596d5e6bec45808f3adc2
ilham25 <ilhamadiputra4@gmail.com>
2021-01-30 11:52:06 +0700

    soal 4 init

---
+++ soal3.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// rl.question("masukkan nama", (name) => {
+//   console.log(name);
+// });
+
+const diamond = `
+    *
+   * *
+  *   *
+   * *
+    *
+`;
+
+const star = "* ";
+for (let index = 0; index < 5; index++) {}
